1614c57b7238c6238f91e7c46298724045e27251
Cesar <cesarfrancopro@gmail.com>
2022-07-02 01:35:28 -0500

    able to access and read hn API data, updated CSS styling

--- src/NewsPage.js
+++ src/NewsPage.js
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import NewsCard from "./components/NewsCard";
 
 const NewsPage = () => {
-	const [article, setArticles] = useState([]);
+	const [articles, setArticles] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [totalPages, setTotalPages] = useState(0);
 
 	useEffect(() => {
 		setIsLoading(true);
 		const fetchData = async () => {
 			try {
 				const { data } = await axios.get("http://hn.algolia.com/api/v1/search?");
-				console.log(data);
+				const { hits, nbPages } = data;
+				setArticles(hits);
+				setTotalPages(nbPages);
 			} catch (err) {
 				console.log(err);
 			} finally {
-				setIsLoading(true);
+				setIsLoading(false);
 			}
 		};
 		fetchData();
@@ -23,6 +27,15 @@ const NewsPage = () => {
 	return (
 		<div className="container">
 			<h1>Hacker News</h1>
+			<div className="news-container">
+				{isLoading ? (
+					<p>Loading...</p>
+				) : (
+						articles.map((article) => (
+							<NewsCard article={article} key={article.objectID} />
+						))
+				)}
+			</div>
 		</div>
 	);
 };
